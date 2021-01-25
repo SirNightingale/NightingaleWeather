8bb9ace04bd13b8e134b35d134652d7c6bcc4f7a
SirNightingale <dm1solovyov@mail.ru>
2021-01-25 23:22:14 +0600

    added the ability to register a new user and authorize

--- app/src/main/java/com/dmitrys/nightingaleweather/activity/LoginActivity.java
+++ app/src/main/java/com/dmitrys/nightingaleweather/activity/LoginActivity.java
@@ -1,28 +1,42 @@
 package com.dmitrys.nightingaleweather.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.dmitrys.nightingaleweather.R;
+import com.dmitrys.nightingaleweather.app.AppConfig;
+import com.dmitrys.nightingaleweather.app.AppController;
+import com.dmitrys.nightingaleweather.helper.SQLiteHandler;
 import com.dmitrys.nightingaleweather.helper.SessionManager;
 
-public class LoginActivity extends Activity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private SharedPreferences sPref;
-    private final String SAVED_LOGIN = "saved_login";
-    private final String SAVED_PASSWORD = "saved_password";
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends Activity {
+    private static final String TAG = RegisterActivity.class.getSimpleName();
 
     private Button btnLogin;
     private Button btnLinkToRegister;
     private EditText inputEmail;
     private EditText inputPassword;
     private SessionManager session;
+    private ProgressDialog pDialog;
+    private SQLiteHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +51,13 @@ public class LoginActivity extends Activity {
             startActivity(intent);
             finish();
         }
+
+        // Progress dialog
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+        // SQLite database handler
+        db = new SQLiteHandler(getApplicationContext());
 
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
@@ -74,15 +95,97 @@ public class LoginActivity extends Activity {
     }
 
     /**
-     * function to verify login details in the SharedPreferences
-     */
+     * function to verify login details in db
+     * */
     private void checkLogin(final String email, final String password) {
-        // TODO: working with future DB
-        session.setLogin(true);
+        // Tag used to cancel the request
+        String tag_string_req = "req_login";
 
-        Intent i = new Intent(getApplicationContext(),
-                MainActivity.class);
-        startActivity(i);
-        finish();
+        pDialog.setMessage("Logging in ...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppConfig.URL_LOGIN, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Login Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    // Check for error node in json
+                    if (!error) {
+                        // user successfully logged in
+                        // Create login session
+                        session.setLogin(true);
+
+                        // Now store the user in SQLite
+                        String uid = jObj.getString("uid");
+
+                        JSONObject user = jObj.getJSONObject("user");
+                        String name = user.getString("name");
+                        String email = user.getString("email");
+                        String created_at = user
+                                .getString("created_at");
+
+                        // Inserting row in users table
+                        db.addUser(name, email, uid, created_at);
+
+                        // Launch main activity
+                        Intent intent = new Intent(LoginActivity.this,
+                                MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        // Error in login. Get the error message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
---
+++ app/src/main/java/com/dmitrys/nightingaleweather/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.dmitrys.nightingaleweather.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "https://nightingaleweather.000webhostapp.com/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "https://nightingaleweather.000webhostapp.com/android_login_api/register.php";
+}
