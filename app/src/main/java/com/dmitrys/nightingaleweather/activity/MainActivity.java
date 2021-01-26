dddc3dd22e550ae4f93e1acbeb9969c229a497e7
SirNightingale <dm1solovyov@mail.ru>
2021-01-26 16:10:31 +0600

    added preparation of the template for main activity

--- app/src/main/java/com/dmitrys/nightingaleweather/activity/MainActivity.java
+++ app/src/main/java/com/dmitrys/nightingaleweather/activity/MainActivity.java
@@ -1,16 +1,59 @@
 package com.dmitrys.nightingaleweather.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.dmitrys.nightingaleweather.R;
+import com.dmitrys.nightingaleweather.helper.SQLiteHandler;
+import com.dmitrys.nightingaleweather.helper.SessionManager;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import com.dmitrys.nightingaleweather.activity.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SessionManager session;
+    private SQLiteHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        session = new SessionManager(getApplicationContext());
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logoutUser();
+            }
+        });
+    }
+
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUser();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
